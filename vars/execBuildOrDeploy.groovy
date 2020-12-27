e6283233090f22fe52e794d7ea9be4551d9db5b9
karthickcv <karthickcv10423@gmail.com>
2020-12-27 17:29:37 +0530

    new def added

---
+++ vars/execBuildOrDeploy.groovy
@@ -0,0 +1,42 @@
+def call(String command)
+ 
+  if(command == "build")
+  {
+      pipeline {
+          agent any
+          stages {
+              stage('Build'){
+                  steps{
+                      echo "Build Pipeline Executed !!!"
+                  }
+              }
+          }
+      }
+      else if(command == "deploy")
+      {
+          pipeline {
+              agent any
+              stages {
+                  stage('Build') {
+                      steps {
+                          echo "Deploy Pipeline is Executed !!!"
+                      }
+                  }
+              }
+          }
+      }
+
+      else
+      {
+          pipeline {
+              agent any
+              stages {
+                  stage('Do nothing'){
+                      steps{
+                          echo "Do nothing"
+                      }
+                  }
+              }
+          }
+      }
+  }
